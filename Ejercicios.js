6c5253da79c0ac033d7e9f91e0b5f231c58aea44
Andres Hernandez Zamora <a301540@uach.mx>
2019-02-09 15:04:23 -0700

    Problemas Fin de semana

---
+++ Ejercicios.js
@@ -0,0 +1,8 @@
+function tablero(){
+  let espacio='#'
+  for(let i=0;i<7;i++){
+    console.log(espacio);
+    espacio=espacio+'#';
+  }
+}
+tablero();
---
+++ sumconditionalarrays.js
@@ -0,0 +1,37 @@
+function difference(array, number) {
+  let sum1 = 0, sum2 = 0, i, side1 = 0, side2 = 0,sum=0;
+  let numside1 = []
+
+  for(i=0;i<array.length;i++){
+    sum+=array[i];
+  }
+
+  side2 = (sum - number) / 2;
+
+  for (i = 0; i < array.length; i++) {
+    if (array[i] % 3 == 0) {
+      sum1 += array[i];
+    } else if (array[i] % 5 == 0){
+      sum2 += array[i];
+    } else {
+      if (sum1 + array[i] > side2) {
+        if (sum2 + array[i] > side2) {
+          return 'No'
+        } else {
+          sum2 += array[i];
+        }
+      } else {
+        sum1 += array[i];
+      }
+    }
+  }
+  if(sum1==sum2){
+    return 'Yes'
+  }else{
+    return 'No'
+  }
+}
+
+let array = [5,2,3,0];
+let number = 0;
+difference(array, number);
